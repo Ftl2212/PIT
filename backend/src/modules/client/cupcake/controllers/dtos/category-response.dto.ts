7dec1a4ebf91f9b7878c90f88724dd760700b47e
Ftl2212 <manutencao.fabio@yahoo.com.br>
2025-11-10 11:22:08 -0300

    Delete backend/src/modules/client/cupcake/controllers directory

--- backend/src/modules/client/cupcake/controllers/dtos/category-response.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Category } from '@prisma/client';
-
-export class CategoryResponseDTO implements Category {
-  id: number;
-  name: string;
-  image: string;
-  description: string;
-}
--- backend/src/modules/client/cupcake/controllers/dtos/list-cupcake-response.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PaginationResponseDTO } from 'src/modules/shared/dtos/pagination-response.dto';
-
-import { CupcakeResponseDTO } from './cupcake-response.dto';
-
-export class ListCupcakeResponseDTO extends PaginationResponseDTO<CupcakeResponseDTO> {
-  content: CupcakeResponseDTO[];
-}
